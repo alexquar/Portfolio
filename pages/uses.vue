fcbffaa85c25e27e344b16ada3ae1ab4f877f5d8
alexquar <alexanderquarrie@gmail.com>
2024-03-20 19:36:01 -0400

    setting up proj view

--- pages/uses.vue
+++ pages/uses.vue
@@ -1,29 +1,38 @@
 <template>
-  <h1 class="text-3xl font-bold my-8">My skills</h1>
-  <p class="text-lg">
-    I am often asked about the gear I use as a software developer and content creator. The hardware and software I use is
-    changing all the time so this page will serve as a place for an updated list. If you have a question about anything I
-    use or a suggestion for something I am missing please reach out to me on. Valuable technical skills I have include, but are not limited to C/C++/C#, Javascript/TypeScript, Java, Python, HTML, CSS, SCSS/SASS, Bootstrap, Tailwind, Vue, Nuxt, Django, MongoDB, MySQL, GraphQL, Firebase, Git, Github, Netlify.
-    <a href="https://www.twitter.com/therealdanvega" aria-label="Dan Vega on Twitter">Twitter</a>. The
-    inspiration for this page came from
-    <a href="https://wesbos.com/uses" aria-label="Wes Bos Uses">Wes Bos</a>'s uses page and has been
-    submitted to
-    <a href="https://uses.tech/" aria-label="uses.tech">uses.tech</a>.
-  </p>
+  <div>
+  <h1 class="text-3xl text-center lg:text-left font-bold my-8">My skills</h1>
+  <p class="my-3"> Below is a list of skills that I am comfortable using in a proffesional and personal development environment.</p>
+  <ul class="list-disc mb-5 pb-5">
+    <li class="ml-8 py-1">JavaScript & TypeScript</li>
+    <li class="ml-8 py-1">Vue & React</li>
+    <li class="ml-8 py-1">Nuxt</li>
+    <li class="ml-8 py-1">C, C++ & C#</li>
+    <li class="ml-8 py-1">HTML5 & CSS3</li>
+    <li class="ml-8 py-1">SCSS/SASS</li>
+    <li class="ml-8 py-1">Bootstrap</li>
+    <li class="ml-8 py-1">Tailwindcss</li>
+    <li class="ml-8 py-1">Python</li>
+    <li class="ml-8 py-1">Django</li>
+    <li class="ml-8 py-1">Java</li>
+    <li class="ml-8 py-1">NodeJs & ExpressJs</li>
+  </ul>
 
-  <h1 class="text-3xl font-bold my-8">My skills</h1>
-  <p class="text-lg">
-    I am often asked about the gear I use as a software developer and content creator. The hardware and software I use is
-    changing all the time so this page will serve as a place for an updated list. If you have a question about anything I
-    use or a suggestion for something I am missing please reach out to me on. Valuable technical skills I have include, but are not limited to C/C++/C#, Javascript/TypeScript, Java, Python, HTML, CSS, SCSS/SASS, Bootstrap, Tailwind, Vue, Nuxt, Django, MongoDB, MySQL, GraphQL, Firebase, Git, Github, Netlify.
-    <a href="https://www.twitter.com/therealdanvega" aria-label="Dan Vega on Twitter">Twitter</a>. The
-    inspiration for this page came from
-    <a href="https://wesbos.com/uses" aria-label="Wes Bos Uses">Wes Bos</a>'s uses page and has been
-    submitted to
-    <a href="https://uses.tech/" aria-label="uses.tech">uses.tech</a>.
-  </p>
+  <h1 class="text-3xl text-center lg:text-left font-bold my-8">My Tools</h1>
+  <p class="my-3"> Below is a list of tools that I am comfortable using in a proffesional and personal development environment. </p>
+  <ul class="list-disc mb-5 pb-5">
+    <li class="ml-8 py-1">Firebase</li>
+    <li class="ml-8 py-1">MySQL</li>
+    <li class="ml-8 py-1">MongoDB</li>
+    <li class="ml-8 py-1">GraphQL</li>
+    <li class="ml-8 py-1">Netlify</li>
+    <li class="ml-8 py-1">Git & Github</li>
+    <li class="ml-8 py-1">VS Code, VS, Replit, </li>
+    <li class="ml-8 py-1">Netlify</li>
+    <li class="ml-8 py-1">Microsft Office & Google Workplace </li>
+    <li class="ml-8 py-1">Jira & ITSM</li>
+  </ul>
 
-  <h1 class="text-3xl font-bold mt-8 mb-2">This Portfolio</h1>
+  <h1 class="text-3xl text-center lg:text-left font-bold mt-8 mb-2">This Portfolio</h1>
   <p class="my-3"> Below is a list of technology that I used to create this portfolio if you are looking to do something similar or are just interested</p>
   <ul class="list-disc mb-5 pb-5">
     <li class="ml-8 py-1">Nuxt 3</li>
@@ -32,6 +41,7 @@
     <li class="ml-8 py-1">Netlify (for hosting)</li>
     <li class="ml-8 py-1">Vue, CSS, HTML, TS (obviously)</li>
   </ul>
+</div>
 
 </template>
 
