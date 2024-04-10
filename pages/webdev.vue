7ebdaf895c693c2d69b37a70e7c671ce68f2e42e
alexquar <alexanderquarrie@gmail.com>
2024-04-09 22:31:24 -0400

    projs

--- pages/webdev.vue
+++ pages/webdev.vue
@@ -15,6 +15,7 @@
   
   <script setup>
   import { ref } from 'vue'
+  import uproj from '../assets/uproj.png'
   import helpphoto from '../assets/U-help.png'
   const infos = [{
     name: 'U Help',
@@ -28,26 +29,25 @@
       'Next.js',
       'TailwindCSS',
       'Supabase (auth and db)',
+      'Netlify',
     ],
     details:
       'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
   },{
-    name: 'Basic Tee 6-Pack',
-    website: '#',
+    name: 'U Projects',
+    website: 'https://u-projects-management.web.app/login',
     image:  {
-        src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg',
-        alt: 'Two each of gray, white, and black shirts laying flat.',
+        src: uproj,
+        alt: 'U Projects',
       },
-    description:
-      'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
-    tools: [
-      'Hand cut and sewn locally',
-      'Dyed with our proprietary colors',
-      'Pre-washed & pre-shrunk',
-      'Ultra-soft 100% cotton',
-    ],
     details:
-      'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+      'Project Management system built using React and Firebase (auth, storage, firestore). Allows users to login/signup then create projects and add users to them. Users can then comment on these projects. Users can see all online/offline users.',
+    tools: [
+      'React',
+      'Firebase Auth',
+      'Firebase storage',
+      'Firestore',
+    ],
   },{
     name: 'Basic Tee 6-Pack',
     website: '#',
