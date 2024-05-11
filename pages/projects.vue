18a61ae80aaed1df5552e4173a69ecad282bc609
alexquar <alexanderquarrie@gmail.com>
2024-05-11 16:24:56 -0400

    final updated

--- pages/projects.vue
+++ pages/projects.vue
@@ -2,7 +2,7 @@
 const query = gql`
 {
   viewer {
-    repositories(first: 36, orderBy:{field:CREATED_AT,direction: DESC}) {
+    repositories(first: 33, orderBy:{field:CREATED_AT,direction: DESC}) {
       totalCount
       nodes {
         id
