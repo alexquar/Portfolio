5083ed41feb38f2461c14bcc565922c504a3a93e
alexquar <alexanderquarrie@gmail.com>
2024-05-13 13:14:34 -0400

    text

--- pages/projects.vue
+++ pages/projects.vue
@@ -30,7 +30,7 @@ const { data } = await useAsyncQuery(query);
 
 <template>
   <h1 class="text-3xl my-8">Projects</h1>
-  <p class="text-lg mb-8">Here are some of my projects on <a href="https://github.com/alexquar" class="underline text-indigo-800">GitHub</a>, listed by date created. Projects range from full stack apps and passion projects to smaller concepts pages. Each project is linked. I hope you enjoy having a look at my projects!</p>
+  <p class="text-lg mb-8">Here are some of my projects on <a href="https://github.com/alexquar" class="underline text-indigo-800">GitHub</a>, listed by creation date. Projects range from full stack apps and passion projects to smaller concepts pages. Each project is linked. I hope you enjoy having a look at my projects!</p>
   <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-5  gap-10">
     <div v-for="project in data?.viewer.repositories.nodes" :key="project.id"
       class="p-8 border-4 my-4 rounded-lg hover:bg-gray-50">
