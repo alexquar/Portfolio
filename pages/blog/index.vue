0730a23fbad4b0c660a55ceb13e4d82c541bba5b
alexquar <alexanderquarrie@gmail.com>
2025-08-28 20:26:11 -0400

    home updates

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,12 +1,15 @@
 <script setup>
 const { data: posts } = await useAsyncData('posts', () =>
-  queryContent('/blog').find()
+  queryContent('/blogs').find()
 )
 </script>
 
 <template>
     <h1 class="text-center mb-5 mt-10 md:mt-20 text-7xl font-serif mx-5 md:mx-0">My Blog</h1>       
   <section class="grid md:grid-cols-3 my-8 gap-10">
-    <Post :posts="posts" />
+    <Post v-if="posts && posts.length > 0" :posts="posts" />
+    <p v-else class="col-span-full text-center text-gray-500 text-xl py-10 bg-gray-100 rounded-lg shadow">
+      No blog posts available
+    </p>
   </section>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,4 @@
 <script setup>
-const { data: posts } = await useAsyncData('latest-posts', () =>
-  queryContent('/blog')
-    .sort({ data: 1 })
-    .limit(3)
-    .find()
-)
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -17,6 +11,8 @@ onMounted(() => {
     once: true, // Run animation only once
   });
 });
+
+const showModal = ref(false);
 </script>
 
 <template>
@@ -33,17 +29,59 @@ onMounted(() => {
         hard-working leader who believes anything can be accomplished with organization, determination and collaboration.
       </p>
       <p class="text-lg py-2">
-        I have been writing code for over 5 years now and have a plethora of experience through work, design teams, school, independant study, tutoring and personal projects.
+        I have been writing code for over 6 years now and have a plethora of experience through work, design teams, school, independant study, tutoring and personal projects.
       </p>
       <p class="text-lg py-2">
-        In my personal life I am a sports fan, tech enthousiast, chef, movie critic and animal lover. My technical interests include full stack development, mobile development and machine learning.
+        In my personal life I am a sports fan, tech enthousiast, chef, movie critic and animal lover. My technical interests include full stack development, mobile development and machine learning, but really as long as I am building something cool I am happy!
       </p>
       <p class="text-lg py-2">
-        To learn about my skills please view the "Skills" page or my resume linked below. To learn more about my projects take a look at the web apps, mobile apps or repositories pages.
+        To learn about my skills please view the "Skills" page or my resume linked below. To learn more about my projects take a look at the web apps, mobile apps or repositories pages. 
       </p>
       <p class="text-lg text-center animate-wiggles py-3 my-10">
-        <a target="_blank" href="https://docs.google.com/document/d/e/2PACX-1vTb15Djw-SYk99io1jrWil-gTTHBseXcH1ALEoPjo3Kg3KSiybar_5pjFR11RbqtA/pub" class="bg-gray-900 text-white border-none hover:bg-gray-700 active:bg-gray-500  py-3 px-10 rounded-3xl">My Resume</a>
-        
+        <button
+          class=" text-white px-4 bg-gray-800 hover:bg-gray-700 active:bg-gray-900 p-2 rounded-full transition"
+          @click="showModal = true"
+        >
+          View My Resume
+        </button>
+        <Teleport to="body">
+          <div
+            v-if="showModal"
+            class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+          >
+            <div class="bg-white rounded-lg h-[90vh] shadow-lg max-w-4xl w-full p-6 relative">
+              <!-- title and formatting disclaimer -->
+              <h2 class="text-2xl font-bold mb-4">My Resume</h2>
+              <p class="text-sm text-gray-500 mb-4">
+                (Note: Formatting may vary due to Google Docs embedding, for best experience download the PDF version below)
+              </p>
+              <button
+                class="absolute top-1 right-3  text-gray-500 hover:text-gray-700 text-2xl"
+                @click="showModal = false"
+                aria-label="Close"
+              >
+                &times;
+              </button>
+              <iframe
+                src="https://docs.google.com/document/d/e/2PACX-1vTR5rRQJRGxh4hCQFeSb2PJ7AlGrGKKu-VV1cnoz4sa6DyvLGlU6wtdIRD9B0TLWA/pub?embedded=true"
+                class="w-full h-[75%] border rounded overflow-auto"
+                sandbox="allow-scripts allow-same-origin"
+                allowfullscreen
+                ></iframe>
+                <a
+                class="mt-6 text-white px-4 bg-gray-800 hover:bg-gray-700 active:bg-gray-900 p-2 rounded-full transition inline-block text-center"
+                href="https://docs.google.com/document/d/1DpYM3ZNC261ZI3attx_S56qtFXKhqdUw/export?format=pdf"
+                target="_blank"
+                rel="noopener"
+                download
+                >
+                Download Resume
+                </a>
+            </div>
+            
+          </div>
+          
+        </Teleport>
       </p>
     </div>
     <img src="~/assets/hi.png" class="w-1/2  my-auto md:max-w-sm p-8 mx-auto" />
